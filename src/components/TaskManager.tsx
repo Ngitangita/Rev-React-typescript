07db8d5c176ef64c9b340ca3828e33f9f3279a76
Ngitangita <hei.elise.3gmail.com>
2024-03-08 12:32:17 +0300

    feat: create type tasks

---
+++ src/components/TaskManager.tsx
@@ -0,0 +1,88 @@
+import { nanoid } from "nanoid";
+import { ChangeEvent, useState } from "react";
+import "./TaskManager.css";
+import { Task, UpdateTitle } from "./type";
+
+// TODO: create custom hook to manage task state
+
+export const TaskManager = () => {
+  const [title, setTitle] = useState<string>("");
+  const [searchKeyword, setSearchKeyword] = useState<string>("");
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  // remove task from list
+  const completeTask = (id: string) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  const updateTask = (id: string, taskUpdate: UpdateTitle) => {
+
+    const index = tasks.findIndex((task) => task.id === id);
+    if(index !== -1){
+      const newTasks = tasks.slice();
+      newTasks[index] = { ...newTasks[index], title: taskUpdate.title };
+      setTasks(newTasks);
+    }
+
+  };
+
+  const addTask = () => {
+    if (title.length < 1) {
+      return;
+    }
+
+    const newTask = {
+      // using nanoid to generate unique id
+      id: nanoid(),
+      title,
+    };
+    setTasks((prev) => prev.concat(newTask));
+    setTitle("");
+  };
+
+  const handleSearch = (ev: ChangeEvent<HTMLInputElement>) => {
+    setSearchKeyword(ev.target.value);
+  };
+
+  const filteredTasks = tasks.filter((task) =>
+    task.title.toLowerCase().includes(searchKeyword.toLowerCase()),
+  );
+
+  return (
+    <div className="container">
+      <h1>Task Manager</h1>
+
+      <div>
+        <input type="text" onChange={handleSearch} placeholder="Search Task" />
+      </div>
+
+      <div className="task">
+        <input
+          type="text"
+          value={title}
+          onChange={(ev) => {
+            setTitle(ev.target.value);
+          }}
+        />
+
+        <button onClick={addTask}>Add Task</button>
+      </div>
+
+      <ul className="container">
+        {filteredTasks.map((task) => (
+          <li key={task.id} className="task">
+            <div className="task">
+              <input
+                type="text"
+                placeholder="Add new task"
+                value={task.title}
+                onChange={(e) => updateTask(task.id, { title: e.target.value })}
+              />
+              <button onClick={() => completeTask(task.id)}>Done</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
---
+++ src/components/type.ts
@@ -0,0 +1,7 @@
+export interface UpdateTitle{
+    title: string;
+}
+
+export interface Task extends UpdateTitle{
+    id: string;
+}
