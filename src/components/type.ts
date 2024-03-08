d05cb3c02be6417c39f6be192376ad1ccd9b155f
Ngitangita <hei.elise.3gmail.com>
2024-03-08 13:03:27 +0300

    feat: Integrate the custom useTaskManager hook into the TaskManager component.

--- src/components/type.ts
+++ src/components/type.ts
@@ -1,3 +1,5 @@
+import { ChangeEvent } from "react";
+
 export interface UpdateTitle{
     title: string;
 }
@@ -5,3 +7,16 @@ export interface UpdateTitle{
 export interface Task extends UpdateTitle{
     id: string;
 }
+
+
+export interface ITaskManagerReturnType {
+    tasks: Task[];
+    addTask: () => void;
+    completeTask: (id: string) => void;
+    updateTask: (id: string, taskUpdate: UpdateTitle) => void;
+    handleSearch: (ev: ChangeEvent<HTMLInputElement>) => void;
+    title: string;
+    setTitle: (title: string) => void;
+    deleteTask: (id: string) => void;
+    searchKeyword: string;
+  }
